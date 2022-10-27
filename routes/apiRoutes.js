e2ca25a825adf4fb71491481c68a14ea5200a278
db <jacboysent@gmail.com>
2022-10-27 16:55:58 -0500

    added routes, installed npms, and other js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs')
+
+var uniqid = require('uniqid');
+
+
+// Routes
+module.exports = (app) => {
+
+  // Get desired note.
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+
+  // Add to db.json file 
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uniqid(),
+    };
+
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+
+  });
+
+
+  // Delete notes.
+  app.delete('/api/notes/:id', (req, res) => {
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+    
+  })
+};
